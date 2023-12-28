a4144033e550d2b0204af2fafeee0e3862344721
Ron <ronbena@MacBook-Pro-Ron.local>
2023-12-28 18:33:14 +0200

    CHANGE

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,15 +5,17 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int n = Integer.parseInt(args[0]);
 
-		for(int i = 1; i <= n; i++){
-			for(int j = 1; j <= n; j++){
+		for(int i = 1; i <= n; i++)
+		{
+			for(int j = 1; j <= n; j++)
+			{
 				System.out.print("* ");
 			}
 			System.out.println();
-			if(i % 2 == 1)
-			{
-			System.out.print(" ");
-			}
+				if(i % 2 == 1)
+				{
+				System.out.print(" ");
+				}
 		}
 	}
 }
