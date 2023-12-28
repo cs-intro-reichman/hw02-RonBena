cb0735ae98857de04d470c4f662cd16cc9cb1f88
Ron <ronbena@MacBook-Pro-Ron.local>
2023-12-28 16:40:31 +0200

    Reverse Changes/

--- Reverse.java
+++ Reverse.java
@@ -5,10 +5,17 @@
 public class Reverse {
 	public static void main (String[] args){
 		String word= args[0];
+		int midch = word.length()/2 - 1 ;
+
 		for(int i = word.length()-1; i > -1;i--){
 			System.out.print(word.charAt(i));
 		}
-		int midch = word.length()/2 - 1 ;
+			if(word.length()%2 == 0){
+				midch = word.length()/2 - 1 ;
+			}
+				else{
+					midch = word.length()/2;
+				}
 		System.out.println();
 		System.out.println("The middle character is " + word.charAt(midch));
 	}
