2ea545f7739d0074409930b5f3a086d4474473fd
Ron <ronbena@MacBook-Pro-Ron.local>
2024-01-01 19:22:28 +0200

    stats

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -14,7 +14,71 @@ public class OneOfEachStats {
 		int seed = Integer.parseInt(args[1]);
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
-		
+		int children=0;
+		int chil2 = 0;
+		int chil3 = 0;
+		int chil4 = 0;
+		double sumchildren = 0;
+		double average;
+		String b = "b ";
+		String g = "g ";
+		double rnd = generator.nextDouble();
+		for(int i = 0; i < T; i++)
+		{
+			if(rnd < 0.5)
+					{
+						while ( rnd < 0.5) {
+							//System.err.print(b);
+							rnd =  Math.random();
+							children ++;
+						}
+						children++;
+						//System.err.println(g);
+					
+					}
+					else
+					{
+						while ( rnd >= 0.5) {
+							//System.err.print(g);
+							rnd =  Math.random();
+							children ++;
+						}
+						children ++;
+						//System.err.println(b);
+					}
+			if (children == 2)	
+			{
+				chil2++;
+			}	
+			else if(children == 3)
+			{
+				chil3++;
+			}
+			else if (children >3)
+			{
+				chil4++;
+			}
+			sumchildren += children;
+			children = 0;
+
+		}
+		average = (sumchildren/T);
+		System.out.println("Average: "+ average +" children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + chil2);
+		System.out.println("Number of families with 3 children: " + chil3);
+		System.out.println("Number of families with 4 or more children: " + chil4);
+		if(chil2 >= chil3 && chil2 >= chil4)
+		{
+			System.out.println("The most common number of children is " + chil2 + ".");
+		}
+		else if(chil3 >= chil2 && chil3 >= chil4)
+		{
+			System.out.println("The most common number of children is " + chil3 + ".");
+		}
+		else if(chil4 >= chil2 && chil4 >= chil3)
+		{
+			System.out.println("The most common number of children is " + chil4 + "or more.");
+		}
 		//// In the previous version of this program, you used a statement like:
 		//// double rnd = Math.random();
 		//// Where "rnd" is the variable that stores the generated random value.
